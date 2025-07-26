df089b733af4851f5556b3c6eaa72e25397adaa0
Megan Ellman <meganlellman@gmail.com>
2025-07-25 17:29:06 -0700

    prompts user for animal info and action options

---
+++ index.js
@@ -0,0 +1,35 @@
+import { input, select } from '@inquirer/prompts';
+
+const animals = [];
+
+addAnimal();
+
+async function addAnimal(){
+    const newAnimal = {
+        species: await input({ message: "What species are they?" }),
+        name: await input({ message: "What is their name?" }),
+        age: await input({ message: "How old are they?" }),
+        breed: await input({ message: "What is their breed?" }),
+        specialNote: await input({ message: "Anything else to note?" }),
+    }
+    animals.push(newAnimal);
+    console.log('Animals: ', animals);
+    const nextAction = await select({
+        message: "What would you like to do next?",
+        choices: ["Add another animal", "View available animals", "Generate adoption webpage", "Exit"]
+    });
+    if (nextAction === "Add another animal") {
+        await addAnimal();
+    } else if (nextAction === "View available animals"){ 
+        const viewAnimal = await select({
+            message: "Select an animal to view more details",
+            choices: animals.map((animal) => ({
+                name: `${animal.name}, ${animal.species}`,
+                value: animal
+            }))
+        })
+        console.log(viewAnimal);
+    } else if (nextAction === "Generate adoption webpage"){
+        generateWebpage();
+    }
+}
